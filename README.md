b0bf8655987631a07029c51d521047c05e27238e
Shugmi Shumunov <SShumunov@gmail.com>
2016-08-21 14:19:13 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # message-demo
-Sample project for prospects
+Sample project for Shugmi Shumunov
+
+#Updates I Made
+1) I added bootstrap to the page. I did this to style the page similar to the LLamasoft website.
+2) I added a favicon.
+3) I added a navbar and styled it similarly to the one on the website.
+4) I added a sticky footer that would stick to the bottom of the page.
+5) I added an accordion that would show/hide the project instructions on click.
+6) I used toastr.js to complete the message task. I used it to style, add fade out, and an x on the messages where the user could click on it, or the message to exit out that particular message.
+7) I added a button to describe why I want to join Llamasoft, and why I think I would be a valuable member to the team.
+
+#Interesting things I noticed about the code
+1) The script tags were at the top of the page, and there was no document.ready on the JS page. This could cause problems as the DOM may not be loaded by the time Jquery were used. I put the script tags at the bottom of the body, and added a document.ready as well.
+2) The initial Javascript was written very cleanly. The manner in which variables were written inside of functions would encapsulate those variables and prevent them from interacting with those in the global space.
+
+#Suggestions for improvements
+1) Allow users the ability to customize their messages. This would allow each individual user to gain the most from their messages, not disturb their workflow as much, but still interact with the messages in a meaningful way.
+2) Add a feature where a user could look up old messages. This could be critical if messages were used to deliver sensitive and important information.
+3) Add an ability to set a do not disturb mode. This would allow the user to focus on an important time sensitive task, but important messages sent would be cataloged for the user to reference in the future.
+
+
+#Places where I felt I went above and beyond
+1)I believe I went above in beyond in trying to replicate the page similar to the LLamasoft website.
+2) I tried to give the user the best experience possible, and positioned the messages in a manner where the user could read them without missing the content on the page.
---
+++ js/msg.js
@@ -0,0 +1,115 @@
+//Add a document.ready to ensure that DOM loads before running the js
+$(document).ready(function(){
+
+  var loopHandle = null;
+
+  // The messageSystem object is where you should do all of your work
+  // Use any combination of javascript, HTML and CSS that you feeling
+  // is appropriate
+
+  // Added Toastr Library to display messages
+  toastr.options = {
+    "closeButton": true,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "3000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut",
+  }
+
+  llamaMessages = function(){
+    messageSystem = {
+        showMessage: function(msg) {
+            toastr["info"](msg);
+        }
+    }
+
+    function showMsg() {
+        quotes = [
+        "What we've got here is failure to communicate.",
+        'Go ahead, make my day.',
+        "I've got a bad feeling about this.",
+        "I don't know half of you half as well as I should like; and I like less than half of you half as well as you deserve.",
+        "I find your lack of faith disturbing.",
+        "You're gonna need a bigger boat.",
+        "Tell Mike it was only business.",
+        "I have come here to chew bubble gum and kick ass, and I'm all out of bubble gum."
+        ];
+        messageSystem.showMessage(_.sample(quotes));
+    }
+
+    function loop() {
+        showMsg();
+        var rand = Math.round(Math.random() * (3000 - 500)) + 500;
+        loopHandle = setTimeout(loop, rand);
+      }
+
+    $(function() {
+       $('#msgButton').click(function() {
+           var btn = $(this),
+          btnTxt = btn.text();
+           if (btnTxt === 'Start Messages') {
+               btn.text('Stop Messages');
+               loopHandle = setTimeout(loop, 500);
+           } else {
+               btn.text('Start Messages');
+               clearTimeout(loopHandle);
+               loopHandle = null;
+           }
+       } );
+    });
+  };
+
+  shugMessage = function() {
+    shugMessageSystem = {
+        shugMessage: function(shugMsg) {
+            toastr["info"](shugMsg);
+        }
+    }
+
+    function loop() {
+        shugMsg();
+        var rand = Math.round(Math.random() * (3000 - 500)) + 500;
+        shugHandle = setTimeout(loop, rand);
+      }
+
+    function shugMsg() {
+        qualifications = [
+        "He wants to be a LLama",
+        "He loves the Ann Arbor area",
+        "He is a quick learner",
+        "He has full-stack aspirations",
+        "He wants to learn .NET and C#",
+        "He is a team player.",
+        "He is a life long learner",
+        "He is able to pick up new technologies quickly",
+        "He loves that Llamasoft is a large company with small roots",
+        "He likes that LLamasoft values their employees"
+        ];
+        shugMessageSystem.shugMessage(_.sample(qualifications));
+    }
+
+    $(function() {
+       $('#shugMsgButton').click(function() {
+           var btn = $(this),
+          btnTxt = btn.text();
+           if (btnTxt === 'What Makes Shugmi Qualified?') {
+               btn.text('Wow! He would be a great LLama!');
+               shugHandle = setTimeout(loop, 500);
+           } else {
+               btn.text('What Makes Shugmi Qualified?');
+               clearTimeout(shugHandle);
+               shugHandle = null;
+           }
+       });
+    });
+  };
+});
